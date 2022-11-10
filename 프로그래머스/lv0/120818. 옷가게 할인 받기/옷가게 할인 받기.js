b78783551f0839d1cb6d1298c1dcd1d05ff83aa5
MoonHayden <107612234+MoonHayden@users.noreply.github.com>
2022-11-10 21:41:49 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.js
@@ -0,0 +1,13 @@
+function solution(price) {
+    if (price >= 500000){
+        return Math.floor(price * .8)
+    }
+    else if(price >= 300000 && price < 500000){
+        return Math.floor(price * .9)
+    }
+    else if(price >= 100000 && price < 300000){
+        return Math.floor(price * .95)
+    }
+    else 
+        return price
+}
